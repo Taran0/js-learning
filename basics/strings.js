a8d7576d2e8f18359727ed80bcfd20e74272401b
Balazs Farkas <fabzsi@gmail.com>
2019-05-06 00:35:10 +0200

    strings

---
+++ basics/strings.js
@@ -0,0 +1,8 @@
+let name = 'Balazs Farkas';
+let firstName = 'Balazs';
+let lastName = 'Farkas';
+let fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+
+
---
+++ basics/strings_challenge.js
@@ -0,0 +1,5 @@
+let city = 'Pecs'
+let country = 'Hungary'
+let location = city + ', ' + country
+
+console.log(location)
